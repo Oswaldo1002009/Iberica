e46a78f3c0a4f73a302c1da8efc551831168f572
Oswaldo1002009 <A01206725@itesm.mx>
2019-03-05 14:57:14 -0600

    Configurar DB

--- Iberica/settings.py
+++ Iberica/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'Iberica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.iberica'),
     }
 }
 
